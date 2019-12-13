7d215bdd9ff6621931ea1423d14fee96b590435e
Adrianicko213 <58575709+Adrianicko213@users.noreply.github.com>
2019-12-13 10:10:21 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
-public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+public class Main {
+	public static void main (String[]args) {
+		
+		Taxi x = new Taxi("LUV143");
+		x.run ("ur");
+		x.printCurrentLocation();
+	      //x:____ , y:____
+        x.book(1,1);
+		//x.run("I am leaving on a Ftaxi");
+		System.out.println(x.reachedDestination());
+		//false
+		
+		
+
+				
+	}
 
 }
